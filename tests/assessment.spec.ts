88d4f31c8b612b0ed984ce7e9955e2c55630149a
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-27 18:55:47 +0530

    Assessment Module

---
+++ tests/assessment.spec.ts
@@ -0,0 +1,25 @@
+/*
+
+Scenario
+
+Student takes an online exam.
+
+Example
+
+Question 1
+
+Question 2
+
+Question 3
+
+Submit
+
+Test Cases
+
+Start assessment
+Answer questions
+Submit assessment
+Verify score
+Verify pass/fail
+
+*/
