006fa12b0e820276eb965dc78bca0d2d9499d135
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-27 19:00:19 +0530

    Certificate Generation Module

---
+++ pages/certificatepage.ts
@@ -0,0 +1,14 @@
+/*
+Scenario
+
+After passing the assessment, the student downloads a certificate.
+
+Test Cases
+
+Verify certificate button
+Generate certificate
+Verify student name
+Verify course name
+Verify issue date
+Download PDF
+*/
---
+++ tests/certificate.spec.ts
@@ -0,0 +1,16 @@
+/*
+
+Scenario
+
+After passing the assessment, the student downloads a certificate.
+
+Test Cases
+
+Verify certificate button
+Generate certificate
+Verify student name
+Verify course name
+Verify issue date
+Download PDF
+
+*/
