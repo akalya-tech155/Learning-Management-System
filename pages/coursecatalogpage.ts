d5429de33a020cb128c15a4e778ba902df566677
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-27 18:42:31 +0530

    Course Catalog Module

---
+++ pages/coursecatalogpage.ts
@@ -0,0 +1,29 @@
+/*
+
+Scenario
+
+Student searches available courses.
+
+Examples
+
+Automation Testing
+
+Java
+
+Python
+
+SQL
+
+Playwright
+Test Cases
+Search course
+Filter by category
+Open course details
+Verify instructor
+Verify course duration
+
+
+
+
+
+*/ 
